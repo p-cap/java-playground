c9a0fe3d10ae1a930c142edb2eeae0f87752454d
p-cap <64616459+p-cap@users.noreply.github.com>
2020-07-13 07:19:30 -0700

    Built jar file and edited manifest

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,7 +1,11 @@
 package com.galvanize;
+//this is a comment
 
+//everything is a class
 public class Application {
+
+    //below is a method
     public static void main (String[] args) {
-        System.out.print("Hello World");
+        System.out.print("Hello World");  //statement
     }
 }
